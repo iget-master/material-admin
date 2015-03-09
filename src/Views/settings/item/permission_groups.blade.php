769a95589fb633a7c8a6c400d03e7a8e7e1e56bd
Elias Soares <esoares@iget.com.br>
2015-03-09 17:56:37 -0300

    CSS

--- src/Views/settings/item/permission_groups.blade.php
+++ src/Views/settings/item/permission_groups.blade.php
@@ -4,7 +4,7 @@
 ?>
 
 <div class="row">
-	<div class="col-md-3 label">
+	<div class="col-md-3 title">
 		{!! trans('materialadmin::admin.permission_groups') !!}
 	</div>
 	<div class="col-md-9 content">
