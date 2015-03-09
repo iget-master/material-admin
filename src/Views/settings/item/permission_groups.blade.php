9ded54ce458a14dc9d73e923420d0d698a5355e8
Elias Soares <esoares@iget.com.br>
2015-03-09 18:01:20 -0300

    CSS

--- src/Views/settings/item/permission_groups.blade.php
+++ src/Views/settings/item/permission_groups.blade.php
@@ -9,14 +9,14 @@
 	</div>
 	<div class="col-md-9 content">
 		{!! trans_choice('materialadmin::admin.permission_groups_count', $permission_groups->count()) !!}
-		<ul class="col-md-6">
+		<ul>
 		@foreach ($permission_groups as $permission_group)
 			<li data-id="{!! $permission_group->id !!}">
 				<span class="name">
 					{!! $permission_group->name !!}
 				</span>
-				<a class="modify"><i class="md md-edit"></i></a>
 				<a class="remove"><i class="md md-delete"></i></a>
+				<a class="modify"><i class="md md-edit"></i></a>
 			</li>
 		@endforeach
 		</ul>
