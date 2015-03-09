b2c8d6418909e75743152a50c0432ce46809649c
Elias Soares <esoares@iget.com.br>
2015-03-09 15:46:08 -0300

    Fix

--- src/Views/settings/item/permission_groups.blade.php
+++ src/Views/settings/item/permission_groups.blade.php
@@ -1,8 +1,12 @@
+<?php
+	use IgetMaster\MaterialAdmin\Models\PermissionGroup;
+?>
+
 <div class="row">
 	<div class="col-md-3">
 		{!! trans('materialadmin::admin.permission_groups') !!}
 	</div>
 	<div class="col-md-9">
-		{!! trans_choice('materialadmin::admin.permission_groups_count') !!}
+		{!! trans_choice('materialadmin::admin.permission_groups_count', PermissionGroup::count()) !!}
 	</div>
 </div>
