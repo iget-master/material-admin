d825536c7142c3f41bf0bdfe5b51f2cce532bf99
Elias Soares <esoares@iget.com.br>
2015-04-15 20:24:47 -0300

    Fixed messages migration

--- src/Database/migrations/2015_03_23_170922_create_table_messages.php
+++ src/Database/migrations/2015_03_23_170922_create_table_messages.php
@@ -15,9 +15,9 @@ class CreateTableMessages extends Migration {
 		Schema::create('messages', function($table){
 			$table->increments('id');
 			$table->integer('to_user_id')->unsigned();
-			$table->foreign('to_user_id')->references('id')->on('users');
+			$table->foreign('to_user_id')->references('id')->on('users')->onDelete('cascade');
 			$table->integer('from_user_id')->unsigned()->nullable();
-			$table->foreign('from_user_id')->references('id')->on('users');
+			$table->foreign('from_user_id')->references('id')->on('users')->onDelete('cascade');
 			$table->string('subject', 255);
 			$table->text('message');
 			$table->tinyInteger('read');
