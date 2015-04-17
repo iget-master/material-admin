d3aafee8e5a4b5860dad9460b8901ef56edb3b14
Elias Soares <esoares@iget.com.br>
2015-04-17 13:17:37 -0300

    Changed RestTrait name

--- src/Controllers/RestController.php
+++ src/Controllers/RestController.php
@@ -1,11 +1,11 @@
 <?php namespace IgetMaster\MaterialAdmin\Controllers;
 
+use IgetMaster\MaterialAdmin\Controllers\Traits\RestTrait;
 use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use IgetMaster\MaterialAdmin\Controllers\Traits\Rest;
 
 abstract class RestController extends BaseController {
 
-	use DispatchesCommands, ValidatesRequests, Rest;
+	use DispatchesCommands, ValidatesRequests, RestTrait;
 }
