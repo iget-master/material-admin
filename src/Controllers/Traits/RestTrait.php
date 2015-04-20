868cedcdfe9a7069b11c851410e73e58feaa7fa2
Elias Soares <esoares@iget.com.br>
2015-04-20 12:32:47 -0300

    Sync RestTrait with master

--- src/Controllers/Traits/RestTrait.php
+++ src/Controllers/Traits/RestTrait.php
@@ -60,4 +60,40 @@ trait RestTrait {
 			return \Redirect::back()->withInput()->with('messages', $messages);
 		}
 	}
+
+    public function fillRelationalModel($class, $external_fields, $replace_new_ids, Request $request) {
+        $new_ids = [];
+        if ($request->has($class)) {
+            $request_items = $request->input($class);
+            $class = "App\\Models\\${class}";
+            foreach($request_items as $index=>$item) {
+                $new = true;
+                if (!is_array($item)) {
+                    $item = [$index=>$item];
+                    $new = false;
+                }
+                foreach($item as $id=>$data) {
+                    if (isset($replace_new_ids) && is_array($replace_new_ids)) {
+                        foreach ($replace_new_ids as $field_name=>$replace_ids) {
+                            if (array_key_exists($data[$field_name], $replace_ids)) {
+                                $data[$field_name] = $replace_ids[$data[$field_name]];
+                            }
+                        }
+                    }
+
+                    if (is_array($external_fields)) {
+                        $data = array_merge($data, $external_fields);
+                    }
+
+                    if ($new) {
+                        $new_ids["[new][${id}]"] = $class::create($data)->id;
+                    } else {
+                        $class::findOrFail($id)->fill($data);
+                    }
+                }
+            }
+        }
+
+        return $new_ids;
+    }
 }
