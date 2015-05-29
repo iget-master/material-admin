3517d3aa435653dbb813f60f1a675231b518006b
Elias Soares <esoares@iget.com.br>
2015-05-29 16:27:23 -0300

    Update admin.php

--- lang/en/admin.php
+++ lang/en/admin.php
@@ -2,31 +2,30 @@
 
 return [
 
-    'version_number' => 'v0.7.0',
-    'create' => 'Create new',
+	'create' => 'Create new',
 	'settings' => 'Settings',
 	'security' => 'Security',
 	'security_help' => 'Change all security settings here.',
-
+	
 	'permission_groups' => 'Permission Groups',
 	'permission_groups_count' => 'There is one permission group registered:|There is :count permission groups registered:',
 	'permission_group_name' => 'Name:',
 	'permission_group_roles' => 'Group Roles:',
-
+	
 	'loading' => 'Loading...',
-
+	
 	'action_discard' => 'Discard Changes',
 	'action_save' => 'Save Changes',
 	'action_create' => 'Create',
 	'action_cancel' => 'Cancel',
 	'action_close' => 'Close',
 	'action_done' => 'Done',
-
+	
 	'now_editing' => 'Now Editing',
 	'select_default' => 'Choose an option',
 	'logout' => 'Logout',
 	'tooltip_filter' => 'Show/Hide Filter Options',
-
+	
 	/*
 	 * Permission Group
 	 */
