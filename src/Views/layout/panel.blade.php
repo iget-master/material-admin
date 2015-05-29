32df8aca432b2e30aaf00b29996d6669b0a3368b
Elias Soares <esoares@iget.com.br>
2015-05-29 16:31:25 -0300

    Update panel.blade.php

--- src/Views/layout/panel.blade.php
+++ src/Views/layout/panel.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-    	<title>CDTSys v{!! config('materialadmin::admin.app_version !!}</title>
+    	<title>CDTSys v{!! config('materialadmin::admin.app_version') !!}</title>
     	<meta charset="utf-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
