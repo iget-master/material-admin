6eaa8613081b416d601a8a096b8bfa8140aef5df
Elias Soares <esoares@iget.com.br>
2015-05-28 21:43:11 -0300

    Updated to add getValueColumn()

--- src/Traits/SelectableTrait.php
+++ src/Traits/SelectableTrait.php
@@ -2,12 +2,29 @@
 namespace IgetMaster\MaterialAdmin\Traits;
 
 trait SelectableTrait {
+    /**
+     * Defines the column used by the select option as label
+     * @return string
+     */
     public function getLabelColumn() {
         return $this->name;
     }
 
+    /**
+     * Defines the column used by the select option as value
+     * @return string
+     */
+    public function getValueColumn() {
+        return $this->id;
+    }
+
+    /**
+     * Return an array with all model items as options.
+     * @return array
+     */
     static public function getSelectOptions() {
         $items = self::all();
+        dd($items);
         $options = [""=>trans('materialadmin::admin.select_default')];
         foreach ($items as $item) {
             $options[$item->id] = $item->getLabelColumn();
