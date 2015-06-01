a4b2bb52a5fc28e762fbf618160ee6f73b06ce80
Elias Soares <esoares@iget.com.br>
2015-06-01 17:55:27 -0300

    Created Setting model and table

---
+++ src/Models/Setting.php
@@ -0,0 +1,60 @@
+<?php namespace IgetMaster\MaterialAdmin\Models;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Setting extends Eloquent {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'settings';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'key';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+	public $rules = [
+		'key' => 'required|max:255',
+        'value' => '',
+	];
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['key','value'];
+
+    /**
+     * @param $key
+     */
+    public function get($key) {
+        return static::find($key)->value;
+
+    }
+
+    public function set($key, $value) {
+        static::updateOrCreate(
+            compact($key, $value)
+        );
+    }
+}
