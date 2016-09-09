a1318bbdefda317c21cf0c037d6849ae31ebca30
Elias Soares <esoares@iget.com.br>
2016-09-09 15:06:23 -0300

    Improve L5.2 compability

--- src/Controllers/RestController.php
+++ src/Controllers/RestController.php
@@ -2,12 +2,10 @@
 
 use IgetMaster\MaterialAdmin\Controllers\Traits\RelationalTrait;
 use IgetMaster\MaterialAdmin\Controllers\Traits\RestTrait;
-use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 abstract class RestController extends BaseController
 {
-
-    use DispatchesCommands, ValidatesRequests, RelationalTrait, RestTrait;
+    use ValidatesRequests, RelationalTrait, RestTrait;
 }
