66e6aed806c174000025604f5322a7a66e5e34ee
Elias Soares <esoares@iget.com.br>
2020-02-19 20:11:32 -0300

    (chore) Remove unwanted migrations

--- src/Database/migrations/2015_03_23_170922_create_table_messages.php
+++ src/Database/migrations/2015_03_23_170922_create_table_messages.php
@@ -16,9 +16,7 @@ class CreateTableMessages extends Migration
         Schema::create('messages', function ($table) {
             $table->increments('id');
             $table->integer('to_user_id')->unsigned();
-            $table->foreign('to_user_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('from_user_id')->unsigned()->nullable();
-            $table->foreign('from_user_id')->references('id')->on('users')->onDelete('set null');
             $table->string('subject', 255);
             $table->text('message');
             $table->tinyInteger('read');
